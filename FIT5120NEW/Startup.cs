727a903d67930fc1ab64c31b2c31c347c1f16abf
Wenjie Jin <wjin0011@student.monash.edu>
2022-08-04 13:30:25 +0800

    First Commit

---
+++ FIT5120NEW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5120NEW.Startup))]
+namespace FIT5120NEW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
